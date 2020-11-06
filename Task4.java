4e05dc09d506ba94e40336f60f58ad5ccfbb7e43
Aleksey Onanov <mesquake@gmail.com>
2020-11-06 19:47:34 +0300

    Create Task4.java

---
+++ Task4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Task4 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите радиус: ");
+        double radius = scanner.nextDouble();
+
+        System.out.print("Введите длину хорды: ");
+        double chord = scanner.nextDouble();
+
+        double angle = 2 * Math.asin(chord / (2 * radius));
+
+        double roundSquare = Math.PI * radius * radius;
+
+        double segmentSquare = radius * radius * (angle - Math.sin(angle)) / 2;
+
+        System.out.println("Радиус круга: " + roundSquare);
+        System.out.println("Радиус сегмента: " + segmentSquare);
+        System.out.println("Разница в площади: " + roundSquare / segmentSquare);
+    }
+
+}
