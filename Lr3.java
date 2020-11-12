79411bb12cec173634785546728405c5ef548c11
Aleksey Onanov <mesquake@gmail.com>
2020-11-12 14:45:07 +0300

    Update Lr3.java

--- Lr3.java
+++ Lr3.java
@@ -1,4 +1,4 @@
-package lr3;
+package lab3;
 
 public class Lr3 {
 
@@ -25,44 +25,6 @@ public class Lr3 {
         }
 
         System.out.println("Суммарная площадь: " + totalArea);
-    }
-
-    public static class Rectangle {
-        private float height;
-        private float width;
-
-        public Rectangle(float height, float width) {
-            this.height = height;
-            this.width = width;
-        }
-
-        public Rectangle(float side) {
-            this(side, side);
-        }
-
-        public float getHeight() {
-            return height;
-        }
-
-        public void setHeight(float height) {
-            this.height = height;
-        }
-
-        public float getWidth() {
-            return width;
-        }
-
-        public void setWidth(float width) {
-            this.width = width;
-        }
-
-        public void printSize() {
-            System.out.println(height + " : " + width);
-        }
-
-        public float area() {
-            return height * width;
-        }
     }
 
 }
